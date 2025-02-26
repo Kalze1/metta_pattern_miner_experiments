a5b60286b390326a23edbd362215e869144f4d5d
Kaleab Zegeye <kaleabzegeye1@gmail.com>
2025-02-26 15:24:09 +0300

    task untill feb 26

--- bbb.py
+++
@@ -1,6 +0,0 @@
-from hyperon import V
-var_atom = V('x')
-print(var_atom) # $x
-print(var_atom.get_name()) # x
-print(var_atom.get_metatype()) # AtomKind.VARIABLE
-print(type(var_atom)) # VariableAtom
